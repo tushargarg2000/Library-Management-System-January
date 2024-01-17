e1254574b434d85cfe3946266df7a123fd364d33
Tushar <tushargarg0110@gmail.com>
2024-01-17 23:04:14 +0530

    Mail message added

--- src/main/java/com/example/librarymanagementsystem/Services/AuthorService.java
+++ src/main/java/com/example/librarymanagementsystem/Services/AuthorService.java
@@ -4,6 +4,8 @@ import com.example.librarymanagementsystem.Entities.Author;
 import com.example.librarymanagementsystem.Repository.AuthorRepository;
 import com.example.librarymanagementsystem.RequestDtos.AddAuthorRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +14,29 @@ public class AuthorService {
     @Autowired
     private AuthorRepository authorRepository;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
 
     public String addAuthor(AddAuthorRequest addAuthorRequest){
 
         Author authorEntity = new Author(addAuthorRequest.getAuthorName(),addAuthorRequest.getAuthorAge(),addAuthorRequest.getEmailId());
 
         Author newAuthor =  authorRepository.save(authorEntity);
+
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setSubject("Hi "+newAuthor.getAuthorName()+" !");
+
+        message.setFrom("springacciojob@gmail.com");
+        message.setTo(newAuthor.getEmailId());
+
+
+        message.setText("You have been successfully Registered on our portal !" +
+                "Looking forward for adding more books ");
+
+        mailSender.send(message);
 
         return "Author  with authorId "+newAuthor.getAuthorId()+" has been saved to DB";
     }
